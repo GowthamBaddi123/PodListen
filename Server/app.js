fb92bc98b6b0b1f4fbd36b0a9c5e1cc87b4dcab4
gowthambaddi123 <gowthambaddi254@gmail.com>
2025-08-31 23:11:35 +0530

    channged package verions

--- Server/app.js
+++ Server/app.js
@@ -10,17 +10,17 @@ connectDB()
 app.use(express.json())
 
 
-var whitelist = ['https://pod-listen.vercel.app', 'https://localhost:5173', 'https://pod-listen-nydm9vipa-mahesh10ks-projects.vercel.app']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions))
+// var whitelist = ['https://pod-listen.vercel.app', 'https://localhost:5173', 'https://pod-listen-nydm9vipa-mahesh10ks-projects.vercel.app']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+app.use(cors())
 app.use(express.urlencoded({extended:true}))
 
 
